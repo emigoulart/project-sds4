115ff773e4d645c13f028366dd1141473374a315
Emilene Goulart <emigoulart@gmail.com>
2021-09-07 21:36:35 -0300

    Basic static components

---
+++ frontend/src/components/NavBar/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/emigoulart" target="_blank" rel="noreferrer">Emilene Goulart</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+                    Event promoted by DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
